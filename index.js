a03921d9e34b3b35084b729e092744e03bbd1160
BangalyLeDeveloppeur <condeBangaly580@gmail.com>
2025-06-22 13:42:09 +0200

    filtrage des travaux par categorie

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
-///ciblage des sur le DOM ////
-const gallery = document.querySelector("main");
 const gallerySection = document.querySelector(".gallery");
 const filter = document.querySelector(".filter");
-console.log(filter)
 
-console.log(gallerySection);
-/// fonction init ///////////////////////////////////////////////////////
-async function init() {
-  GetWorks();
-  GetCategories();
-  createBouton();
-  afficherTravaux();
-}
-init();
-
-/// Fonction pour recuperer les travaux dans la base des données///////
 async function GetWorks() {
-  const respons = fetch("http://localhost:5678/api/works");
-  return (await respons).json();
+  const response = await fetch("http://localhost:5678/api/works");
+  return await response.json();
 }
-//// Récuperer les acategories /////////////////////////////////////////////////////
+
 async function GetCategories() {
-  const respons = await fetch("http://localhost:5678/api/categories");
-  return await respons.json();
+  const response = await fetch("http://localhost:5678/api/categories");
+  return await response.json();
 }
-/// création des bouton dynamique///
+
 async function createBouton() {
   const dataCategori = await GetCategories();
-  console.log(dataCategori);
 
+  // Bouton Tous
+  const allBtn = document.createElement("button");
+  allBtn.textContent = "Tous";
+  allBtn.id = "0";
+  filter.appendChild(allBtn);
 
   dataCategori.forEach((category) => {
     const btn = document.createElement("button");
     btn.textContent = category.name;
     btn.id = category.id;
     filter.appendChild(btn);
-    
   });
 }
 
-async function afficherTravaux() {
-  gallerySection.innerHTML = " ";
-  const travaux = await GetWorks();
-  console.log(travaux);
+async function afficherTravaux(travaux = null) {
+  gallerySection.innerHTML = "";
+  if (!travaux) {
+    travaux = await GetWorks();
+  }
+
   travaux.forEach((work) => {
     const figure = document.createElement("figure");
     const img = document.createElement("img");
@@ -50,8 +41,35 @@ async function afficherTravaux() {
 
     img.src = work.imageUrl;
     figcaption.textContent = work.title;
+
     figure.appendChild(img);
     figure.appendChild(figcaption);
     gallerySection.appendChild(figure);
   });
 }
+
+async function filtrageTravaux() {
+  const Trav = await GetWorks();
+
+  const AllButton = document.querySelectorAll(".filter button");
+  AllButton.forEach((catego) => {
+    catego.addEventListener("click", (e) => {
+      const btnId = e.target.getAttribute("id");
+
+      if (btnId !== "0") {
+        const filtercategorie = Trav.filter((work) => work.categoryId == btnId);
+        afficherTravaux(filtercategorie);
+      } else {
+        afficherTravaux(Trav);
+      }
+    });
+  });
+}
+///fonction pour afficher les differentes fonction///////////////////////////
+async function init() {
+  await createBouton();
+  await afficherTravaux();
+  await filtrageTravaux();
+}
+
+init();
